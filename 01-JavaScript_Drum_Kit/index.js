8c4bef040d7c9986ee3006dd0d2045eab300ab42
WeiLocus <marsuo52@gmail.com>
2023-04-13 12:27:51 +0800

    finish day1

---
+++ 01-JavaScript_Drum_Kit/index.js
@@ -0,0 +1,23 @@
+
+window.addEventListener('keydown', playSound)
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`)
+  const key = document.querySelector(`.key[data-key=${e.code}]`)
+  if (!audio) return
+  audio.currentTime = 0;
+  audio.play()
+  key.classList.add('playing')
+}
+
+// use transition end event to stop animation
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend',removeTransition))
+
+function removeTransition (e) {
+  if (e.propertyName !== 'transform') return
+    e.target.classList.remove('playing')
+}
+
+
+
